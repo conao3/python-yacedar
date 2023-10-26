7369565db0d5c3c93eca6daba7c9a4cea2c0f560
Naoya Yamashita <conao3@gmail.com>
2023-10-27 01:57:59 +0900

    list and put list

--- sample/tinytodo/src/tinytodo/main.py
+++ sample/tinytodo/src/tinytodo/main.py
@@ -1,10 +1,39 @@
 import argparse
 import cmd
+from typing import NewType, Optional
 
+
+TUserName = NewType('TUserName', str)
+TListName = NewType('TListName', str)
+TTaskName = NewType('TTaskName', str)
+TTask = NewType('TTask', str)
+TList = NewType('TList', dict[TTaskName, list[TTask]])
 
 class TinyTodoShell(cmd.Cmd):
     intro = 'Welcome to the tinytodo shell. Type help or ? to list commands.\n'
     prompt = 'tinytodo> '
+
+    __login: Optional[TUserName] = TUserName('guest')
+    __lists: dict[TUserName, TList] = {}
+
+    def do_login(self, line: str) -> None:
+        self.__login = TUserName(line)
+
+    def do_logout(self, line: str) -> None:
+        self.__login = None
+
+    def do_list_lists(self, line: str) -> None:
+        self.columnize(list(self.__lists.keys()))
+
+    def do_put_list(self, line: str) -> None:
+        if line in self.__lists:
+            print('List already exists')
+        else:
+            self.__lists[TUserName(line)] = TList({})
+
+    def postcmd(self, stop: bool, line: str) -> bool:
+        self.prompt = 'tinytodo' + (f'({self.__login})' if self.__login else '') + '> '
+        return super().postcmd(stop, line)
 
 
 def parse_args() -> argparse.Namespace:
@@ -15,4 +44,7 @@ def parse_args() -> argparse.Namespace:
 def main() -> None:
     args = parse_args()
 
-    TinyTodoShell().cmdloop()
+    try:
+        TinyTodoShell().cmdloop()
+    except KeyboardInterrupt:
+        print('')
