f2606684cabf8528e902a7dcc5123a19604f85db
Naoya Yamashita <conao3@gmail.com>
2023-10-27 01:24:10 +0900

    introduce cmd.Cmd

--- sample/tinytodo/src/tinytodo/main.py
+++ sample/tinytodo/src/tinytodo/main.py
@@ -1,2 +1,18 @@
+import argparse
+import cmd
+
+
+class TinyTodoShell(cmd.Cmd):
+    intro = 'Welcome to the tinytodo shell. Type help or ? to list commands.\n'
+    prompt = 'tinytodo> '
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    return parser.parse_args()
+
+
 def main() -> None:
-    print('hello')
+    args = parse_args()
+
+    TinyTodoShell().cmdloop()
