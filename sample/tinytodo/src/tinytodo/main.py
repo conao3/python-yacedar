122623db433352d5c855f96c23d5e22342197554
Naoya Yamashita <conao3@gmail.com>
2023-10-29 03:50:08 +0900

    reconstruct data structure

--- sample/tinytodo/src/tinytodo/main.py
+++ sample/tinytodo/src/tinytodo/main.py
@@ -12,14 +12,18 @@ def trap[T](fn: Callable[[], T]) -> tuple[T, Literal[None]] | tuple[Literal[None
 
 
 TUserName = NewType('TUserName', str)
-TTaskName = NewType('TTaskName', str)
-TListName = NewType('TListName', str)
+TTaskNo = NewType('TTaskNo', int)
+TListNo = NewType('TListNo', int)
 
 
 class TTask:
-    def __init__(self, task_name: TTaskName) -> None:
+    def __init__(self, task_no: TTaskNo, task_name: str) -> None:
+        self.task_no = task_no
         self.task_name = task_name
         self.status = False
+
+    def disp_oneline(self) -> str:
+        return f'{self.task_no}) {"[x]" if self.status else "[ ]"} {self.task_name}'
 
     def disp(self) -> str:
         return f'{"[x]" if self.status else "[ ]"} {self.task_name}'
@@ -29,17 +33,47 @@ class TList:
     def __init__(
         self,
         owner: TUserName,
-        list_name: TListName,
-        tasks: list[TTask]
+        list_no: int,
+        list_name: str,
+        tasks: dict[TTaskNo, TTask] = {}
     ) -> None:
         self.owner = owner
+        self.list_no = list_no
         self.list_name = list_name
         self.tasks = tasks
         self.readers: list[TUserName] = []
         self.editors: list[TUserName] = []
 
+        self.counter_task_no = 0
+
+    def new_task_no(self) -> TTaskNo:
+        ret = TTaskNo(self.counter_task_no)
+        self.counter_task_no += 1
+        return ret
+
+    def disp_oneline(self) -> str:
+        return f'{self.owner}/{self.list_no}: {self.list_name}'
+
     def disp(self) -> str:
-        return f'{self.owner}/{self.list_name}'
+        return f'''\
+Owner: {self.owner}
+List No: {self.list_no}
+List Name: {self.list_name}
+Tasks:
+{"\n".join(f"  {task.disp_oneline()}" for task in self.tasks.values())}'''
+
+
+class TUser:
+    def __init__(self) -> None:
+        self.name: TUserName
+        self.lists: dict[TListNo, TList] = {}
+
+        self.counter_list_no = 0
+
+    def new_list_no(self) -> TListNo:
+        ret = TListNo(self.counter_list_no)
+        self.counter_list_no += 1
+        return ret
 
 
 class InvalidArgumentError(Exception):
@@ -51,49 +85,32 @@ class TinyTodoShell(cmd.Cmd):
     prompt = 'tinytodo(guest)> '
 
     __login: TUserName = TUserName('guest')
-    __lists: list[TList] = []
+    __lists: dict[TUserName, TUser] = {}
 
     def _assert_args_len(self, expected_len: int, args: list[str], help: str) -> None:
         if len(args) != expected_len:
             raise InvalidArgumentError(help)
 
-    def _get_list(self, owner: TUserName, list_name: TListName) -> tuple[int, TList]:
-        for i, list_ in enumerate(self.__lists):
-            if list_.owner == owner and list_.list_name == list_name:
-                return i, list_
+    def _parse_list_name(self, arg: str) -> tuple[TUserName, TListNo]:
+        if '/' in arg:
+            owner_, list_no_ = arg.split('/', 1)
 
-        raise RuntimeError('List not found')
-
-    def _get_task(self, owner: TUserName, list_name: TListName, task_name: TTaskName) -> tuple[TList, int, TTask]:
-        _, list_ = self._get_list(owner, list_name)
-
-        for i, task in enumerate(list_.tasks):
-            if task.task_name == task_name:
-                return list_, i, task
-
-        raise RuntimeError('Task not found')
-
-    def _parse_list_name(self, list_name: str) -> tuple[TUserName, TListName]:
-        if '/' in list_name:
-            owner, list_name = list_name.split('/', 1)
-
-            if '/' in list_name:
-                raise RuntimeError('Invalid list name: Use of "/" in list name is not allowed')
-
-            owner = TUserName(owner)
-            list_name = TListName(list_name)
+            owner = TUserName(owner_)
+            arg = list_no_
 
         else:
             owner = self.__login
-            list_name = TListName(list_name)
 
-        return owner, list_name
+        return owner, TListNo(int(arg))
 
     def do_login(self, line: str) -> None:
         args = shlex.split(line)
         self._assert_args_len(1, args, 'login <user_name>')
 
         self.__login = TUserName(args[0])
+
+        if self.__login not in self.__lists:
+            self.__lists[self.__login] = TUser()
 
     def do_logout(self, line: str) -> None:
         args = shlex.split(line)
@@ -105,80 +122,64 @@ class TinyTodoShell(cmd.Cmd):
         args = shlex.split(line)
         self._assert_args_len(1, args, 'put_list <list_name>')
 
-        list_name = TListName(args[0])
+        user = self.__lists[self.__login]
+        list_no = user.new_list_no()
 
-        if '/' in list_name:
-            raise RuntimeError('Invalid list name: Use of "/" in list name is not allowed')
+        user.lists[list_no] = TList(self.__login, list_no, args[0])
 
-        if any((self.__login, list_name) == elm for elm in [(elm.owner, elm.list_name) for elm in self.__lists]):
-            raise RuntimeError('List already exists')
-
-        self.__lists.append(TList(self.__login, list_name, []))
-
-    def do_list_lists(self, line: str) -> None:
+    def do_get_lists(self, line: str) -> None:
         args = shlex.split(line)
-        self._assert_args_len(0, args, 'list_lists')
+        self._assert_args_len(0, args, 'get_lists')
 
-        for list_ in self.__lists:
-            if not list_.owner == self.__login:
-                continue
+        for list_ in self.__lists[self.__login].lists.values():
+            print(list_.disp_oneline())
 
-            print(list_.disp())
+    def do_get_list(self, line: str) -> None:
+        args = shlex.split(line)
+        self._assert_args_len(1, args, 'get_list [<owner_name>/]<list_no>')
+
+        list_no = TListNo(int(args[0]))
+
+        print(self.__lists[self.__login].lists[list_no].disp())
 
     def do_delete_list(self, line: str) -> None:
         args = shlex.split(line)
-        self._assert_args_len(1, args, 'delete_list [<owner_name>/]<list_name>')
+        self._assert_args_len(1, args, 'delete_list <list_no>')
 
-        owner, list_name = self._parse_list_name(args[0])
+        list_no = TListNo(int(args[0]))
 
-        i, _ = self._get_list(owner, list_name)
-        del self.__lists[i]
+        del self.__lists[self.__login].lists[list_no]
 
     def do_put_task(self, line: str) -> None:
         args = shlex.split(line)
-        self._assert_args_len(2, args, 'put_task [<owner_name>/]<list_name> <task_name>')
+        self._assert_args_len(2, args, 'put_task <list_no> <task_name>')
 
-        owner, list_name = self._parse_list_name(args[0])
-        task_name = TTaskName(args[1])
+        list_no = TListNo(int(args[0]))
 
-        task, _ = trap(lambda: self._get_task(owner, list_name, task_name))
+        user = self.__lists[self.__login]
+        list_ = user.lists[list_no]
+        task_no = list_.new_task_no()
 
-        if task is not None:
-            raise RuntimeError('Task already exists')
-
-        _, list_ = self._get_list(owner, list_name)
-        list_.tasks.append(TTask(task_name))
-
-    def do_list_tasks(self, line: str) -> None:
-        args = shlex.split(line)
-        self._assert_args_len(1, args, 'list_tasks [<owner_name>/]<list_name>')
-
-        owner, list_name = self._parse_list_name(args[0])
-
-        _, list_ = self._get_list(owner, list_name)
-        for task in list_.tasks:
-            print(task.disp())
+        list_.tasks[task_no] = TTask(task_no, args[1])
 
     def do_toggle_task(self, line: str) -> None:
         args = shlex.split(line)
-        self._assert_args_len(2, args, 'toggle_task [<owner_name>/]<list_name> <task_name>')
+        self._assert_args_len(2, args, 'toggle_task [<owner_name>/]<list_no> <task_no>')
 
-        owner, list_name = self._parse_list_name(args[0])
-        task_name = TTaskName(args[1])
+        list_no = TListNo(int(args[0]))
+        task_no = TTaskNo(int(args[1]))
 
-        _, _, task = self._get_task(owner, list_name, task_name)
-
+        task = self.__lists[self.__login].lists[list_no].tasks[task_no]
         task.status = not task.status
 
     def do_delete_task(self, line: str) -> None:
         args = shlex.split(line)
-        self._assert_args_len(2, args, 'delete_task [<owner_name>/]<list_name> <task_name>')
+        self._assert_args_len(2, args, 'delete_task [<owner_name>/]<list_no> <task_no>')
 
-        owner, list_name = self._parse_list_name(args[0])
-        task_name = TTaskName(args[1])
+        list_no = TListNo(int(args[0]))
+        task_no = TTaskNo(int(args[1]))
 
-        list_, i, _ = self._get_task(owner, list_name, task_name)
-        del list_.tasks[i]
+        del self.__lists[self.__login].lists[list_no].tasks[task_no]
 
     def onecmd(self, line: str) -> bool:
         try:
